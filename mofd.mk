10d9e09151fa9f535dbc2fc9db48ea1f57ebcbb5
Victor Linfield <vilinfield@gmail.com>
2016-07-17 17:20:06 -0230

    FIX

--- mofd.mk
+++ mofd.mk
@@ -95,8 +95,8 @@ ADDITIONAL_DEFAULT_PROPERTIES += ro.dalvik.vm.native.bridge=libhoudini.so
 
 # Keyhandler
 PRODUCT_PACKAGES += \
-    CMActions \
-    com.cyanogenmod.keyhandler
+#    CMActions \
+#    com.cyanogenmod.keyhandler
 
 # Key layout files
 PRODUCT_COPY_FILES += \
